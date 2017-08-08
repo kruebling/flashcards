c04d13357b14790f968a7041d3caf49ea135e813
Keegan Ruebling and Mark Helt <mark@>
2017-08-08 13:44:54 -0700

    add js functionality to all flashcards, styled cards with color and animation

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $("#1").click(function() {
+    $("#1.flashtitle").toggle(800);
+    $("#1.flashinfo").toggle(800);
+  });
+  $("#2").click(function() {
+    $("#2.flashtitle").toggle(800);
+    $("#2.flashinfo").toggle(800);
+  });
+  $("#3").click(function() {
+    $("#3.flashtitle").toggle(800);
+    $("#3.flashinfo").toggle(800);
+  });
+  $("#4").click(function() {
+    $("#4.flashtitle").toggle(800);
+    $("#4.flashinfo").toggle(800);
+  });
+  $("#5").click(function() {
+    $("#5.flashtitle").toggle(800);
+    $("#5.flashinfo").toggle(800);
+  });
+  $("#6").click(function() {
+    $("#6.flashtitle").toggle(800);
+    $("#6.flashinfo").toggle(800);
+  });
+  $("#7").click(function() {
+    $("#7.flashtitle").toggle(800);
+    $("#7.flashinfo").toggle(800);
+  });
+  $("#8").click(function() {
+    $("#8.flashtitle").toggle(800);
+    $("#8.flashinfo").toggle(800);
+  });
+  $("#9").click(function() {
+    $("#9.flashtitle").toggle(800);
+    $("#9.flashinfo").toggle(800);
+  });
+  $("#10").click(function() {
+    $("#10.flashtitle").toggle(800);
+    $("#10.flashinfo").toggle(800);
+  });
+  $("#11").click(function() {
+    $("#11.flashtitle").toggle(800);
+    $("#11.flashinfo").toggle(800);
+  });
+  $("#12").click(function() {
+    $("#12.flashtitle").toggle(800);
+    $("#12.flashinfo").toggle(800);
+  });
+  $("#13").click(function() {
+    $("#13.flashtitle").toggle(800);
+    $("#13.flashinfo").toggle(800);
+  });
+  $("#14").click(function() {
+    $("#14.flashtitle").toggle(800);
+    $("#14.flashinfo").toggle(800);
+  });
+  $("#15").click(function() {
+    $("#15.flashtitle").toggle(800);
+    $("#15.flashinfo").toggle(800);
+  });
+  $("#16").click(function() {
+    $("#16.flashtitle").toggle(800);
+    $("#16.flashinfo").toggle(800);
+  });
+  $("#17").click(function() {
+    $("#17.flashtitle").toggle(800);
+    $("#17.flashinfo").toggle(800);
+  });
+  $("#18").click(function() {
+    $("#18.flashtitle").toggle(800);
+    $("#18.flashinfo").toggle(800);
+  });
+  $("#19").click(function() {
+    $("#19.flashtitle").toggle(800);
+    $("#19.flashinfo").toggle(800);
+  });
+});
